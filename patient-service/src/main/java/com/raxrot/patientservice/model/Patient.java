06a538cc9ec04338dde47fb239e378d1454b2fb0
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-11-23 22:03:24 +0000

    change gen type from AUTO to UUID

--- patient-service/src/main/java/com/raxrot/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/raxrot/patientservice/model/Patient.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Entity
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @NotNull
