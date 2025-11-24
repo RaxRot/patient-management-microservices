0a0f579997bedd5b0898b361e60e84792e71328d
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-11-24 20:34:44 +0000

    add PatientResponseDTO

---
+++ patient-service/src/main/java/com/raxrot/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,14 @@
+package com.raxrot.patientservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private LocalDate dateOfBirth;
+}
