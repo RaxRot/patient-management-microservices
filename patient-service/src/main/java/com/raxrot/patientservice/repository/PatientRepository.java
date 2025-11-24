1661b4ddc677759d934f0a10186f077d6e18aad5
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-11-24 20:21:58 +0000

    add PatientRepository

---
+++ patient-service/src/main/java/com/raxrot/patientservice/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.raxrot.patientservice.repository;
+
+import com.raxrot.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
