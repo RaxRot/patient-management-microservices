33dcfea3476a7826b3bd2cc4bc3edb7c1f36aad9
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-11-24 21:27:20 +0000

    add toModel map

--- patient-service/src/main/java/com/raxrot/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/raxrot/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.raxrot.patientservice.mapper;
 
+import com.raxrot.patientservice.dto.PatientRequestDTO;
 import com.raxrot.patientservice.dto.PatientResponseDTO;
 import com.raxrot.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDto(Patient patient){
@@ -12,5 +15,23 @@ public class PatientMapper {
         dto.setAddress(patient.getAddress());
         dto.setDateOfBirth(patient.getDateOfBirth());
         return dto;
+    }
+
+    public static Patient toModel(PatientRequestDTO dto){
+        Patient patient = new Patient();
+        patient.setName(dto.getName());
+        patient.setEmail(dto.getEmail());
+        patient.setAddress(dto.getAddress());
+        patient.setDateOfBirth(parseDate(dto.getDateOfBirth()));
+        patient.setRegisteredDate(parseDate(dto.getRegisteredDate()));
+        return patient;
+    }
+
+    private static LocalDate parseDate(String date) {
+        try {
+            return LocalDate.parse(date);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Date must be in format yyyy-MM-dd");
+        }
     }
 }
