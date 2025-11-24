e729bd9d338fde174efe41831ab3cee8c8fba3e6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-11-24 21:48:48 +0000

    add existsByEmail check

--- patient-service/src/main/java/com/raxrot/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/raxrot/patientservice/service/PatientService.java
@@ -2,10 +2,12 @@ package com.raxrot.patientservice.service;
 
 import com.raxrot.patientservice.dto.PatientRequestDTO;
 import com.raxrot.patientservice.dto.PatientResponseDTO;
+import com.raxrot.patientservice.exception.ApiException;
 import com.raxrot.patientservice.mapper.PatientMapper;
 import com.raxrot.patientservice.model.Patient;
 import com.raxrot.patientservice.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,11 @@ public class PatientService {
     private final PatientRepository patientRepository;
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new ApiException("Patient with email " + patientRequestDTO.getEmail() + " already exists",
+                    HttpStatus.BAD_REQUEST);
+        }
+
         Patient newPatient = PatientMapper.toModel(patientRequestDTO);
         Patient savedPatient = patientRepository.save(newPatient);
         return PatientMapper.toDto(savedPatient);
